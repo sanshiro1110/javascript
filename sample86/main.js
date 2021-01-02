153846e05275b6a98bb707844be4d634b1d32968
sanshiro1110 <sanshirou1110_0508@yahoo.co.jp>
2021-01-02 19:39:17 +0900

    hero-slider created

---
+++ sample86/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider('.swiper-container');
+  hero.start();
+
+  // setTimeout(() => {
+  //   hero.stop();
+  // }, 5000);
+});
+
+
+
